b4df98a4f4f095b9d7e24f096bc2edeeb0a2f205
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-29 19:40:03 -0300

    Design do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddCircle, MdKeyboardArrowRight } from 'react-icons/md';
+import { Container, Content, Meetup, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <h2>Meus meetups</h2>
+          <button type="button">
+            <MdAddCircle size={22} color="#fff" />
+            Novo meetup
+          </button>
+        </nav>
+        <Meetup>
+          <p>Meetup de React Native</p>
+          <div>
+            <p>24 de Junho, às 20h</p>
+            <MdKeyboardArrowRight size={22} color="#fff" />
+          </div>
+        </Meetup>
+        <Meetup>
+          <p>Meetup de React Native</p>
+          <div>
+            <p>24 de Junho, às 20h</p>
+            <MdKeyboardArrowRight size={22} color="#fff" />
+          </div>
+        </Meetup>
+        <Meetup>
+          <p>Meetup de React Native</p>
+          <div>
+            <p>24 de Junho, às 20h</p>
+            <MdKeyboardArrowRight size={22} color="#fff" />
+          </div>
+        </Meetup>
+        <Meetup>
+          <p>Meetup de React Native</p>
+          <div>
+            <p>24 de Junho, às 20h</p>
+            <MdKeyboardArrowRight size={22} color="#fff" />
+          </div>
+        </Meetup>
+        <Meetup>
+          <p>Meetup de React Native</p>
+          <div>
+            <p>24 de Junho, às 20h</p>
+            <MdKeyboardArrowRight size={22} color="#fff" />
+          </div>
+        </Meetup>
+      </Content>
+    </Container>
+  );
 }
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 900px;
+  margin: 30px auto;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    margin-bottom: 20px;
+
+    h2 {
+      font-size: 30px;
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      border: none;
+      background: #d44059;
+      border-radius: 4px;
+      padding: 10px 10px;
+      color: #fff;
+
+      svg {
+        padding-right: 5px;
+      }
+    }
+  }
+`;
+
+export const Meetup = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px 0;
+  color: #fff;
+  background: rgba(0, 0, 0, 0.15);
+  padding: 20px 10px;
+  border-radius: 4px;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      margin-left: 10px;
+    }
+
+    p {
+      color: #999;
+      font-size: 13px;
+    }
+  }
+
+  p {
+    font-weight: bold;
+    font-size: 15px;
+    margin-left: 20px;
+  }
+`;
