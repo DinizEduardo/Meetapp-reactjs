01904e513adaa0b56b609e5ada274f5c404196a3
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-29 17:37:50 -0300

    Design do login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import Logo from '../../assets/logo.svg';
+
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <form>
+      <img src={Logo} alt="Meetapp" />
+      <input type="text" placeholder="Digite seu e-mail" />
+      <input type="text" placeholder="Sua senha" />
+      <button type="button">Entrar</button>
+      <a href="">Criar conta grátis</a>
+    </form>
+  );
 }
--- src/pages/_layout/auth/styles.js
+++ src/pages/_layout/auth/styles.js
@@ -1,7 +1,59 @@
 import styled from 'styled-components';
 
+import { darken } from 'polished';
+
 export const Wrapper = styled.div`
-  background: #7159c1;
+  background: #4b2c50;
+  background: linear-gradient(180deg, #000, #422b46);
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  width: 300px;
+  img {
+    margin-bottom: 30px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+
+    input {
+      font-size: 14px;
+      flex: 1;
+      margin: 5px;
+      border: 0;
+      padding: 10px 15px;
+      border-radius: 4px;
+      background: #261c2c;
+      color: #fff;
+
+      &::placeholder {
+        color: #aaa;
+      }
+    }
+
+    button {
+      flex: 1;
+      margin: 15px 5px;
+      border: 0;
+      padding: 10px;
+      background: #d44059;
+      border-radius: 4px;
+      color: #fff;
+      font-size: 20px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#d44059')};
+      }
+    }
+
+    a {
+      color: #aaa;
+    }
+  }
+`;
